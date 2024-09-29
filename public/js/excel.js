305b7e3f3714c43ab4007b6c26553679edb3ff38
farruhzoirov <fzoirov29@gmail.com>
2024-09-29 11:48:19 +0500

    commit

--- public/js/excel.js
+++ public/js/excel.js
@@ -7,7 +7,7 @@ if (user && user?.name && user?.phone && user?.time) {
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    fetch('https://script.google.com/macros/s/AKfycbyYMLTf9gatN-a1mCHCc8Sxvyluq3oGo5ggO53quRqusSl74gBucxGlCqL3ixgWnZAF/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzBcTYQJCC9xZyK7HEXmMVHNOH0c40QqBdrSbOqnOGgEiiCgjJgRpoGcMAx_fUIln_M/exec', {
         method: 'POST',
         body: formData
     })
